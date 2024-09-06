8561ac1baa3dc3a3637f96fcaf359b70d5addb72
l0cass <piffer.lucas0@gmail.com>
2024-09-06 17:07:52 -0300

    chore: index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      sm: "2rem",
+      lg: "4rem",
+      xl: "5rem",
+      DEFAULT: "1rem",
+    },
+    backgroundImage: {
+      "index-image": "url('assets/images/index-background.jpg')",
+      "mobile-index-image": "url('assets/images/mobile-index-background.jpg')",
+    },
+  },
 };
