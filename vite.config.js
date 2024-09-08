e8fb47ff3737b308f4599dcba6c73f675d99808c
l0cass <piffer.lucas0@gmail.com>
2024-09-08 19:12:43 -0300

    fix routes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
 
-export default defineConfig({ server: { host: "0.0.0.0" } });
+export default defineConfig({
+  server: { host: "0.0.0.0" },
+  plugins: [Inspect()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        quotation: resolve(__dirname, "quotation/index.html"),
+      },
+    },
+  },
+});
